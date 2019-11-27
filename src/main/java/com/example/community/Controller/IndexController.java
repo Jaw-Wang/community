e6666632e0061796c550adbeaeab27fa37340338
Jaw-Wang <765082108@qq.com>
2019-11-27 13:28:59 +0800

    引入lombok，添加头像

--- src/main/java/com/example/community/Controller/IndexController.java
+++ src/main/java/com/example/community/Controller/IndexController.java
@@ -25,6 +25,7 @@ public class IndexController {
     public String index(HttpServletRequest request){
 
         Cookie [] cookies =request.getCookies();
+        if(cookies!=null &&cookies.length!=0)
         for(Cookie cookie:cookies){
             if(cookie.getName().equals("token")){
                 String token =cookie.getValue();
