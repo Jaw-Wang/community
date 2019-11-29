322814d9f031760f02c81a11577af8783593401f
Jaw-Wang <765082108@qq.com>
2019-11-29 17:14:38 +0800

    分页

--- src/main/java/com/example/community/Controller/IndexController.java
+++ src/main/java/com/example/community/Controller/IndexController.java
@@ -1,19 +1,17 @@
 package com.example.community.Controller;
 
-import com.example.community.Dto.QuestionDTO;
-import com.example.community.Mapper.QuestionMapper;
+import com.example.community.Dto.PaginationDTO;
 import com.example.community.Mapper.UserMapper;
-import com.example.community.Model.Question;
 import com.example.community.Model.User;
 import com.example.community.Service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
-import java.util.List;
 
 /**
  * @author create by Jaw
@@ -30,23 +28,25 @@ public class IndexController {
 
     @GetMapping("/")
     public String index(HttpServletRequest request,
-                        Model model){
+                        Model model,
+                        @RequestParam(name = "page", defaultValue = "1") Integer page,
+                        @RequestParam(name = "size", defaultValue = "5") Integer size) {
 
-        Cookie [] cookies =request.getCookies();
-        if(cookies!=null &&cookies.length!=0)
-        for(Cookie cookie:cookies){
-            if(cookie.getName().equals("token")){
-                String token =cookie.getValue();
-                User user =userMapper.findByToken(token);
-                if(user!=null){
-                    request.getSession().setAttribute("user", user);
+        Cookie[] cookies = request.getCookies();
+        if (cookies != null && cookies.length != 0)
+            for (Cookie cookie : cookies) {
+                if (cookie.getName().equals("token")) {
+                    String token = cookie.getValue();
+                    User user = userMapper.findByToken(token);
+                    if (user != null) {
+                        request.getSession().setAttribute("user", user);
+                    }
+                    break;
                 }
-                break;
             }
-        }
 
-        List<QuestionDTO> questionList=questionService.list();
-        model.addAttribute("questionList",questionList);
+        PaginationDTO pagination = questionService.list(page,size);
+        model.addAttribute("pagination", pagination);
         return "index";
     }
 }
--- src/main/java/com/example/community/Service/QuestionService.java
+++ src/main/java/com/example/community/Service/QuestionService.java
@@ -1,5 +1,6 @@
 package com.example.community.Service;
 
+import com.example.community.Dto.PaginationDTO;
 import com.example.community.Dto.QuestionDTO;
 import com.example.community.Mapper.QuestionMapper;
 import com.example.community.Mapper.UserMapper;
@@ -22,19 +23,36 @@ public class QuestionService {
     private UserMapper userMapper;
 
     //查询问题列表
-    public List<QuestionDTO> list() {
-        List<Question> questions=questionMapper.list();
-        List<QuestionDTO> questionDTOList=new ArrayList<>();
-        for (Question question:questions){
-            User user= userMapper.findById(question.getCreator());
-            QuestionDTO questionDTO=new QuestionDTO();
+    public PaginationDTO list(Integer page, Integer size) {
+
+        PaginationDTO paginationDTO = new PaginationDTO();
+        Integer totalCount = questionMapper.count();
+        paginationDTO.setPagination(totalCount, page, size);
+        //判断越界
+        if (page < 1) {
+            page = 1;
+        }
+        if (page > paginationDTO.getTotalPage()) {
+            page = paginationDTO.getTotalPage();
+        }
+        //size*(page-1)
+        Integer offset = size * (page - 1);
+
+        List<Question> questions = questionMapper.list(offset, size);
+        List<QuestionDTO> questionDTOList = new ArrayList<>();
+
+        for (Question question : questions) {
+            User user = userMapper.findById(question.getCreator());
+            QuestionDTO questionDTO = new QuestionDTO();
             //把question对象放到questionDTO对象
-            BeanUtils.copyProperties(question,questionDTO);
+            BeanUtils.copyProperties(question, questionDTO);
             questionDTO.setUser(user);
             questionDTOList.add(questionDTO);
             //老版本
             /*questionDTO.setId(question.getId());*/
         }
-        return questionDTOList;
+        paginationDTO.setQuestions(questionDTOList);
+
+        return paginationDTO;
     }
 }
